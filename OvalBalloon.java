f1793e26ecbf3366bd62edb86bdb2598ae108317
Jill Seman <jillseman@Jills-MacBook-Air.local>
2015-09-05 13:17:03 -0700

    Oval draw method fix

--- OvalBalloon.java
+++ OvalBalloon.java
@@ -1,12 +1,31 @@
-package Balloon;
-
 import java.awt.Color;
+import java.awt.Graphics;
 
-public class OvalBalloon extends Balloon 
-{
+public class OvalBalloon extends Balloon
+{   
+private int xCenter;
+private int yCenter;
+private int radius;
+private Color color;
+
+
 	public OvalBalloon()
 	{}
 	
 	public OvalBalloon(int x, int y, int r, Color c)
 	{  super(x, y, r, c);  }
+	
+	public void draw(Graphics g, boolean makeItFilled)
+	  {
+
+		  System.out.println("Spot #oval");
+	    g.setColor(color);
+	    g.fillOval(100, 100, 20, 20);
+	    if (makeItFilled)
+	      g.fillOval(xCenter - radius,
+	                 yCenter - radius, 4*radius, 2*radius);
+	    else
+	      g.drawOval(xCenter - radius,
+	                 yCenter - radius, 4*radius, 2*radius);
+	  }
 }
