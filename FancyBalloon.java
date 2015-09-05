8402d9008515bfec3ebe85ae285ee0e1f63f84c9
Jill Seman <jillseman@Jills-MacBook-Air.local>
2015-09-05 10:50:04 -0700

    second push via term

---
+++ FancyBalloon.java
@@ -0,0 +1,12 @@
+package Balloon;
+
+import java.awt.Color;
+
+public class FancyBalloon extends Balloon 
+{
+	public FancyBalloon()
+	{}
+	
+	public FancyBalloon(int x, int y, int r, Color c)
+	{  super(x, y, r, c);  }
+}
